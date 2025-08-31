3b274366169df9b2386813759c753252cb3d9a6f
Beetle <drquinsey@gmail.com>
2025-08-31 07:20:05 -0400

    cleaned up traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,24 @@
-use std::ops::Mul;
-
-use traits::{Bimap, First, Second};
+use traits::Bimap;
 
 pub mod traits;
 
-impl<A, B, A2> First<A, B, A2> for (A, B)
+impl<A, A2, B, B2, F, G> Bimap<A, A2, B, B2, F, G> for (A, B)
 where
-    A: Clone,
-    B: Clone,
+    F: FnOnce(A) -> A2,
+    G: FnOnce(B) -> B2,
 {
-    fn fst(&self) -> A {
-        self.0.clone()
+    fn first(self, f: F) -> (A2, B) {
+        let (a, b) = self;
+        (f(a), b)
     }
 
-    fn first<F>(&self, f: F) -> (A2, B)
-    where
-        F: FnOnce(A) -> A2,
-    {
-        (f(self.0.clone()), self.1.clone())
-    }
-}
-
-impl<A, B, B2> Second<A, B, B2> for (A, B)
-where
-    A: Clone,
-    B: Clone,
-{
-    fn snd(&self) -> B {
-        self.1.clone()
+    fn second(self, g: G) -> (A, B2) {
+        let (a, b) = self;
+        (a, g(b))
     }
 
-    fn second<F>(&self, f: F) -> (A, B2)
-    where
-        F: FnOnce(B) -> B2,
-    {
-        (self.0.clone(), f(self.1.clone()))
+    fn bimap(self, f: F, g: G) -> (A2, B2) {
+        let (a, b) = self;
+        (f(a), g(b))
     }
-}
-
-impl<A, A2, B, B2> Bimap<A, A2, B, B2> for (A, B) where Self: First<A, B, A2> + Second<A, B, B2> {}
-
-fn square_then_multiply<T>(t: &(T, T)) -> T
-where
-    T: Clone + Mul<Output = T>,
-{
-    let square = |x: T| -> T { x.clone() * x };
-    let (a, b) = t.bimap(square, square);
-    a * b
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,32 +1,15 @@
-/// Enables retrieval and mapping over the first element of a pair
-pub trait First<A, B, A2> {
-    /// Return a reference to the first element of a pair
-    fn fst(&self) -> A;
-
-    /// Returns a new pair with the a function applied to the first element
-    fn first<F>(&self, f: F) -> (A2, B)
-    where
-        F: FnOnce(A) -> A2;
-}
-
-/// Enables retrieval and mapping over the second element of a pair
-pub trait Second<A, B, B2>: Sized {
-    /// Return the second element of a pair
-    fn snd(&self) -> B;
-
-    /// Returns a new pair with the a function applied to the second element
-    fn second<F>(&self, f: F) -> (A, B2)
-    where
-        F: FnOnce(B) -> B2;
-}
-
 /// Enables mapping over both elements of a pair at once
-pub trait Bimap<A, A2, B, B2>: First<A, B, A2> + Second<A, B, B2> {
-    fn bimap<F, G>(&self, f: F, g: G) -> (A2, B2)
-    where
-        F: FnOnce(A) -> A2,
-        G: FnOnce(B) -> B2,
-    {
-        (f(self.fst()), g(self.snd()))
-    }
+pub trait Bimap<A, A2, B, B2, F, G>
+where
+    F: FnOnce(A) -> A2,
+    G: FnOnce(B) -> B2,
+{
+    /// Returns `self` with `f` applied to its first element.
+    fn first(self, f: F) -> (A2, B);
+
+    /// Returns `self` with `g` applied to its second element.
+    fn second(self, g: G) -> (A, B2);
+
+    /// Returns `self` with `f` applied to its first element and `g` applied to its second element.
+    fn bimap(self, f: F, g: G) -> (A2, B2);
 }
